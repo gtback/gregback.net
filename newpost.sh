cd1650097cdd8670ca3213046d91b3585cf82b82
Greg Back <gtback@gmail.com>
2021-06-26 15:15:20 -0500

    Update newpost script

--- newpost.sh
+++ newpost.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-cat <<EOF
-Title:
+# Usage:
+# ./newpost.sh This is the Title of the new post
+
+set -euo pipefail
+
+TITLE="$*"
+FILENAME="$(echo "$TITLE" | tr "[:upper:]" "[:lower:]" | tr "[:blank:]" "-").md"
+
+echo "$TITLE"
+echo "$FILENAME"
+
+cat > "content/${FILENAME}" <<EOF
+Title: $TITLE
 Date: $(date +"%F %R")
 Modified: $(date +"%F %R")
 Category:
